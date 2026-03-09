b0ca59c0dbe075539bc1342407e3e8d9ddd62d10
Simran Shaikh <simranshaikhxyz@gmail.com>
2026-03-09 22:41:53 +0530

    server.js

--- server.js
+++ server.js
@@ -365,7 +365,7 @@ app.post('/api/seed-data', async (req, res) => {
 });
 
 // Server start karo
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
